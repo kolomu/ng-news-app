0825eb9b7d6a066a11bed822fe0f59f5093c0267
kev <kevin-olomu@Web.de>
2018-10-09 18:34:25 +0200

    implmented app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'news-app';
+  mArticles: Array<any>; // holds news articles
+  mSources: Array<any>; // holds news sources
+
+  constructor(private newsApi: NewsApiService) {
+    console.log('app component constructor called');
+  }
+
+  ngOnInit() {
+    // load articles
+    this.newsApi.initArticles().subscribe(data => this.mArticles = data['articles']);
+    // load news sources
+    this.newsApi.initSources().subscribe(data => this.mSources = data['sources']);
+  }
+
+  searchArticles(source) {
+    console.log(`selected source is ${source}`);
+    this.newsApi.getArticlesById(source).subscribe(data => this.mArticles = data['articles']);
+  }
 }
