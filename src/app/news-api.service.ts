66f0cbefc734fe262d66b242324aae5b6b14d6da
kev <kevin-olomu@Web.de>
2018-10-09 18:30:14 +0200

    implemented news service

--- src/app/news-api.service.ts
+++ src/app/news-api.service.ts
@@ -1,10 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { APIKEY } from '../private';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsApiService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  // prepare left-side menu with some news resources
+  initSources() {
+    return this.http.get(`https://newsapi.org/v2/sources?language=en&apiKey=${APIKEY}`);
+  }
+
+  initArticles() {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${APIKEY}`);
+  }
+
+  getArticlesById(source: string) {
+    return this.http.get(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${APIKEY}`);
+  }
 }
